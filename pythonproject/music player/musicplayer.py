1c42b422825e87621341115afe91f610da50bb54
Adarsh Gautam <78965473+adarshgautam777@users.noreply.github.com>
2023-05-01 13:47:06 +0530

    Update musicplayer.py

--- pythonproject/music player/musicplayer.py
+++ pythonproject/music player/musicplayer.py
@@ -1,2 +1,13 @@
 from playsound import playsound
-playsound('C:\\Users\\HP\\Downloads\\Csk.mp3')
+
+print("No. of available songs\n1.Csk\n2.Jaadui\n3.O Bedardeya\n4.Apna Bana le")
+order = input("Enter the music which you want to play : ")
+if "Csk" in order:
+    playsound('C:\\Users\\HP\\Downloads\\Csk.mp3')
+elif "Jaadui" in order:
+    playsound('C:\\Users\\HP\\Downloads\\Jaadui.mp3')
+elif "O Bedardeya" in order:
+    playsound('C:\\Users\\HP\\Downloads\\O Bedardeya.mp3')
+elif "Apna Bana le" in order:
+    playsound('C:\\Users\\HP\\Downloads\\Apna Bana le.mp3')
+    
